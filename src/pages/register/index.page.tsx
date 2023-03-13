67eea43bfc57c86e620bf97f574b44c366b3019b
João Vitor Lima <jvolima2004@gmail.com>
2023-03-13 17:45:40 -0300

    feat: save user id on cookies

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { AxiosError } from 'axios'
 import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
@@ -50,7 +51,12 @@ export default function Register() {
         username: data.username,
       })
     } catch (error) {
-      console.log(error)
+      if (error instanceof AxiosError && error?.response?.data?.message) {
+        alert(error.response.data.message)
+        return
+      }
+
+      console.error(error)
     }
   }
 
