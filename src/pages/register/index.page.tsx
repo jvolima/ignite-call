37958b21704801295a125ca1edab409572858b14
João Vitor Lima <jvolima2004@gmail.com>
2023-03-13 17:23:38 -0300

    feat: register user in prisma

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -5,6 +5,7 @@ import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { api } from '../../lib/axios'
 import { Container, Form, FormError, Header } from './styles'
 
 const registerFormSchema = z.object({
@@ -43,7 +44,14 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
